3e90fe39d945315c17afcc097c5dff706af60f66
xmeix <lamiaboualouache@gmail.com>
2022-05-24 12:12:35 +0100

    fixed smthg

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
-import History from "./components/History/History";
-import TodoForm from "./components/todoForm/TodoForm"
+//import History from "./components/History/History";
+import TodoForm from "./components/todoForm/TodoForm";
 import TodoList from "./components/todoList/TodoList";
 function App() {
   return (
     <div className="App">
       <Header />
       {/** here the todo form */}
-      <TodoForm/>
+      <TodoForm />
       {/** here the todo list */}
-      <TodoList/>
+      <TodoList />
       {/** here the completed todo list history */}
-      <History/>
+      {/* <History /> */}
     </div>
   );
 }
